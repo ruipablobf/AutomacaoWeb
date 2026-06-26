44ee9fde6d460e73d543d0f45c4061624d0df0d1
Rui Pablo <124592526+ruipablobf@users.noreply.github.com>
2026-06-26 14:24:35 -0300

    Format code and adjust spacing in ProdutoStepDefinitions

--- AutomacaoAPI/src/test/java/steps/ProdutoStepDefinitions.java
+++ AutomacaoAPI/src/test/java/steps/ProdutoStepDefinitions.java
@@ -6,7 +6,7 @@ import org.example.produtoapi.Produto;
 
 public class ProdutoStepDefinitions {
 
-    Produto produto = new Produto();
+    Produto produto =  new Produto();
 
     @Dado("que eu realizo a inserção do produto {string} na api com sucesso")
     public void adicionarProdutoAPI(String nomeProduto) {
@@ -14,7 +14,7 @@ public class ProdutoStepDefinitions {
     }
 
     @Então("busco um produto pelo id {string} que também está cadastrado na api")
-    public void buscarProdutoAPI(String nome) {
+    public void buscarProdutoAPI(String nome){
         produto.buscarProdutoAPI(nome);
     }
 }
