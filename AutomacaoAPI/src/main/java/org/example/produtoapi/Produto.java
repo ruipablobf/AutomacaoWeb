570bca9c265ff3c648413148a43415c3dc3b2d5c
Rui Pablo <124592526+ruipablobf@users.noreply.github.com>
2026-06-26 14:23:16 -0300

    Format Produto class and methods for consistency

--- AutomacaoAPI/src/main/java/org/example/produtoapi/Produto.java
+++ AutomacaoAPI/src/main/java/org/example/produtoapi/Produto.java
@@ -1,14 +1,14 @@
 package org.example.produtoapi;
 
-public class Produto {
+public class Produto{
 
-    ProdutoController produtoController = new ProdutoController();
+    ProdutoController produtoController =  new ProdutoController();
 
-    public void buscarProdutoAPI(String idProduto) {
+    public void buscarProdutoAPI(String idProduto){
         produtoController.buscarProduto(idProduto);
     }
 
-    public void adicionarProdutoAPI(String nomeProduto) {
+    public void adicionarProdutoAPI(String nomeProduto){
         produtoController.adicionarProduto(nomeProduto);
     }
 
